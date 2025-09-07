ac441309354e1295d32587fa1fcfcf76c81ca8c5
Agustin-Nicolas-Torres <agustinnicolastorres963@gmail.com>
2025-09-06 23:16:50 -0300

    Tarea 1 Agustin Torres - 39031

---
+++ objetos/ejercicios/ejercicio1/clases/Autor.ts
@@ -0,0 +1,7 @@
+export class Autor{
+    constructor(
+        public nombre: string,
+        public bibliografia: string,
+        public nacimiento: number
+    ){}
+}
--- objetos/ejercicios/ejercicio1/clases/Libro.ts
+++ objetos/ejercicios/ejercicio1/clases/Libro.ts
@@ -1,7 +1,12 @@
+import { Socio } from "./Socio.js";
+import {Autor} from "./Autor.js"
+
 export class Libro {
+  private _reservas: Socio[] = [];
+
   constructor(
     private _titulo: string,
-    private _autor: string,
+    private _autor: Autor,
     private _isbn: string
   ) {}
 
@@ -13,5 +18,8 @@ export class Libro {
   }
   get isbn() {
     return this._isbn;
+  }
+  get reservas():Socio[] {
+    return this._reservas
   }
 }
