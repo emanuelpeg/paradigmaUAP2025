88a52d2af488d64742ac804fb661e99bc0cc574e
Pablo Bernat <pablobernat25@gmail.com>
2025-09-22 19:17:33 -0300

    Guia 1 completada

--- funcional/ejercicios/ejercicio1/src/Main.elm
+++ funcional/ejercicios/ejercicio1/src/Main.elm
@@ -1,11 +1,33 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
 
+import Html exposing (Html, div, text, br)
 
 main : Html msg
 main =
-    text "Hello, Elm!"
+    div []
+        [ text ("power 2 3 = " ++ String.fromInt (power 2 3))
+        , br []
+        , text ("factorial 5 = " ++ String.fromInt (factorial 5))
+        , br []
+        , text ("fibonacciExponential 10 = " ++ String.fromInt (fibonacciExponential 10))
+        , br []
+        , text ("fibonacciLinear 10 = " ++ String.fromInt (fibonacciLinear 10))
+        , br []
+        , text ("pascalTriangle 2 4 = " ++ String.fromInt (pascalTriangle 2 4))
+        , br []
+        , text ("gcd 48 18 = " ++ String.fromInt (gcd 48 18))
+        , br []
+        , text ("countDigits 12345 = " ++ String.fromInt (countDigits 12345))
+        , br []
+        , text ("sumDigits 123 = " ++ String.fromInt (sumDigits 123))
+        , br []
+        , text ("isPalindrome 12321 = " ++ String.fromBool (isPalindrome 12321))
+        , br []
+        , text ("reverseNumber 12345 = " ++ String.fromInt (reverseNumber 12345))
+        , br []
+        , text ("isBalanced \"((()))()\" = " ++ String.fromBool (isBalanced "((()))()"))
+        ]
 
 
 add : Int -> Int -> Int
@@ -21,91 +43,97 @@ multiply a b =
 -- Ejercicio 1: Función Potencia
 power : Int -> Int -> Int
 power a b =
-    -- TODO: Implementar función potencia
-    0
+    if b == 0 then 1
+    else a * power a (b - 1)
 
 
 -- Ejercicio 2: Factorial
 factorial : Int -> Int
 factorial n =
-    -- TODO: Implementar factorial
-    0
+    if n <= 1 then 1 else n * factorial (n - 1)
 
 
 -- Ejercicio 3: Fibonacci
 fibonacciExponential : Int -> Int
 fibonacciExponential n =
-    -- TODO: Implementar fibonacci exponencial
-    0
+    if n == 0 then 0
+    else if n == 1 then 1
+    else fibonacciExponential (n - 1) + fibonacciExponential (n - 2)
 
 
 fibonacciLinear : Int -> Int
 fibonacciLinear n =
-    -- TODO: Implementar fibonacci lineal con acumuladores
-    0
+    fibonacciHelper n 0 1
 
 
 fibonacciHelper : Int -> Int -> Int -> Int
 fibonacciHelper n acc1 acc2 =
-    -- TODO: Función auxiliar para fibonacci lineal
-    0
+    if n == 0 then acc1
+    else fibonacciHelper (n - 1) acc2 (acc1 + acc2)
 
 
 -- Ejercicio 4: Triángulo de Pascal
 pascalTriangle : Int -> Int -> Int
 pascalTriangle x y =
-    -- TODO: Implementar triángulo de Pascal
-    0
+    if x == 0 || x == y then 1
+    else pascalTriangle (x - 1) (y - 1) + pascalTriangle x (y - 1)
 
 
 -- Ejercicio 5: Máximo Común Divisor (MCD)
 gcd : Int -> Int -> Int
 gcd a b =
-    -- TODO: Implementar algoritmo euclidiano
-    0
+    if b == 0 then
+        abs a
+    else
+        gcd b (modBy b a)
 
 
 -- Ejercicio 6: Contar Dígitos
 countDigits : Int -> Int
 countDigits n =
-    -- TODO: Implementar contador de dígitos
-    0
+    let
+        m = abs n
+    in
+    if m < 10 then 1 else 1 + countDigits (m // 10)
 
 
 -- Ejercicio 7: Suma de Dígitos
 sumDigits : Int -> Int
 sumDigits n =
-    -- TODO: Implementar suma de dígitos
-    0
+    let
+        m = abs n
+    in
+    if m < 10 then m else (m % 10) + sumDigits (m // 10)
 
 
 -- Ejercicio 8: Verificar Palíndromo
 isPalindrome : Int -> Bool
 isPalindrome n =
-    -- TODO: Implementar verificador de palíndromo
-    False
+    let m = abs n in m == reverseNumber m
 
 
 reverseNumber : Int -> Int
 reverseNumber n =
-    -- TODO: Implementar función para invertir número
-    0
+    reverseHelper (abs n) 0
 
 
 reverseHelper : Int -> Int -> Int
 reverseHelper n acc =
-    -- TODO: Función auxiliar para invertir número
-    0
+    if n == 0 then acc else reverseHelper (n // 10) (acc * 10 + (n % 10))
 
 
 -- Ejercicio 9: Paréntesis Balanceados
 isBalanced : String -> Bool
 isBalanced str =
-    -- TODO: Implementar verificador de paréntesis balanceados
-    False
+    isBalancedHelper (String.toList str) 0
 
 
 isBalancedHelper : List Char -> Int -> Bool
 isBalancedHelper chars counter =
-    -- TODO: Función auxiliar para verificar paréntesis balanceados
-    False
+    case chars of
+        [] -> counter == 0
+        c :: cs ->
+            if counter < 0 then False
+            else if c == '(' then isBalancedHelper cs (counter + 1)
+            else if c == ')' then isBalancedHelper cs (counter - 1)
+            else isBalancedHelper cs counter
