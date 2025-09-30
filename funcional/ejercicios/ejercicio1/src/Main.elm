7812d01e6925f0c2a0ff970aebd500626071b5af
EveUAP <evelyn.inostroza@uap.edu.ar>
2025-09-29 21:43:23 -0300

    Subo TP - módulo Clase2

--- funcional/ejercicios/ejercicio1/src/Main.elm
+++ funcional/ejercicios/ejercicio1/src/Main.elm
@@ -1,8 +1,12 @@
 module Main exposing (..)
 
+<<<<<<< HEAD
 import Html exposing (Html, text)
 import Html exposing (b)
 import Html.Attributes exposing (accept)
+=======
+import Html exposing (Html, a, text)
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 main : Html msg
@@ -12,107 +16,225 @@ main =
 
 add : Int -> Int -> Int
 add a b =
-    a + b
+    if b == 0 then
+        a
+
+    else
+        add (a + 1) (b - 1)
 
 
 multiply : Int -> Int -> Int
 multiply a b =
     if b == 0 then
         0
+<<<<<<< HEAD
     else
         a * multiply a (b - 1)
+=======
+
+    else if b == 1 then
+        a
+
+    else
+        a + multiply a (b - 1)
+
+
+multiply2 : Int -> Int -> Int -> Int
+multiply2 a b acc =
+    if b == 0 then
+        acc
+
+    else
+        multiply2 a (b - 1) (acc + a)
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 -- Ejercicio 1: Función Potencia
+
+
 power : Int -> Int -> Int
 power a b =
     if b == 0 then
         1
+<<<<<<< HEAD
     else
         a * power a (b - 1)
     
+=======
+
+    else
+        a * power a (b - 1)
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 -- Ejercicio 2: Factorial
+
+
 factorial : Int -> Int
 factorial n =
+<<<<<<< HEAD
     if n == 0 then
         1
     else                                    
         n * factorial (n - 1)
+=======
+    if n <= 1 then
+        1
+
+    else
+        n * factorial (n - 1)
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 -- Ejercicio 3: Fibonacci
+
+
 fibonacciExponential : Int -> Int
 fibonacciExponential n =
+<<<<<<< HEAD
     if n <= 0 then
         0
     else if n == 1 then
         1
+=======
+    if n <= 1 then
+        n
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
     else
         fibonacciExponential (n - 1) + fibonacciExponential (n - 2)
 
 
 fibonacciLinear : Int -> Int
 fibonacciLinear n =
+<<<<<<< HEAD
     if n <= 0 then
         0
     else
         fibonacciHelper n 0 1
+=======
+    fibonacciHelper n 0 1
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 fibonacciHelper : Int -> Int -> Int -> Int
 fibonacciHelper n acc1 acc2 =
     if n == 0 then
         acc1
+<<<<<<< HEAD
     else 
         fibonacciHelper(n - 1) acc2 (acc1 + acc2)
+=======
+
+    else if n == 1 then
+        acc2
+
+    else
+        fibonacciHelper (n - 1) acc2 (acc1 + acc2)
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 -- Ejercicio 4: Triángulo de Pascal
+
+
 pascalTriangle : Int -> Int -> Int
 pascalTriangle x y =
+<<<<<<< HEAD
     if x == 0 || x == y then 1
     else pascalTriangle(x - 1) (y - 1) + pascalTriangle x (y - 1)
+=======
+    if x == 0 || x == y then
+        1
+
+    else
+        pascalTriangle (x - 1) (y - 1) + pascalTriangle x (y - 1)
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 
 -- Ejercicio 5: Máximo Común Divisor (MCD)
+
+
 gcd : Int -> Int -> Int
 gcd a b =
     if b == 0 then
+<<<<<<< HEAD
         abs a 
     else
         gcd b (modBy b a) 
+=======
+        abs a
+
+    else
+        gcd b (modBy b a)
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 -- Ejercicio 6: Contar Dígitos
+
+
 countDigits : Int -> Int
 countDigits n =
     if n < 0 then
+<<<<<<< HEAD
         countDigits(abs n)
     else if n < 10 then
         1
     else
         1 + countDigits (n//10)
+=======
+        countDigits (-1 * n)
+
+    else if n < 10 then
+        1
+
+    else
+        1 + countDigits (n // 10)
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 -- Ejercicio 7: Suma de Dígitos
+
+
 sumDigits : Int -> Int
 sumDigits n =
+<<<<<<< HEAD
     if n < 10 then
         n
     else
         modBy 10 n + sumDigits(n//10)
+=======
+    if n < 0 then
+        sumDigits (-1 * n)
+
+    else if n < 10 then
+        n
+
+    else
+        modBy 10 n + sumDigits (n // 10)
+
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 -- Ejercicio 8: Verificar Palíndromo
+
+
 isPalindrome : Int -> Bool
 isPalindrome n =
+<<<<<<< HEAD
     if n < 0 then
         False
     else
         (abs n == reverseNumber (abs n))
+=======
+    n >= 0 && n == reverseNumber n
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
 
 reverseNumber : Int -> Int
@@ -122,21 +244,20 @@ reverseNumber n =
 
 reverseHelper : Int -> Int -> Int
 reverseHelper n acc =
+<<<<<<< HEAD
     if n == 0 then
         acc
     else if n < 10 then
         acc*10 + n
     else reverseHelper (n//10) (acc * 10 + modBy 10 n)
+=======
+    if n < 10 then
+        acc * 10 + n
+>>>>>>> dceaf4e5d25ad98a024dfa8c1781ebd0eabfcf42
 
-
--- Ejercicio 9: Paréntesis Balanceados
-isBalanced : String -> Bool
-isBalanced str =
-    -- TODO: Implementar verificador de paréntesis balanceados
-    False
-
-
-isBalancedHelper : List Char -> Int -> Bool
-isBalancedHelper chars counter =
-    -- TODO: Función auxiliar para verificar paréntesis balanceados
-    False
+    else
+        let
+            digit =
+                modBy 10 n
+        in
+        reverseHelper (n // 10) (acc * 10 + digit)
---
+++ funcional/ejercicios/ejercicio2/src/Clase2.elm
@@ -0,0 +1,281 @@
+module Clase2 exposing (..)
+
+import List exposing (..)
+import Maybe exposing (withDefault)
+import Debug
+
+head : List a -> a
+head list =
+    Maybe.withDefault (Debug.todo "head called on empty list") (List.head list)
+
+
+tail : List a -> List a
+tail list =
+    Maybe.withDefault [] (List.tail list)
+
+
+isEmpty : List a -> Bool
+isEmpty list =
+    List.isEmpty list
+
+
+{-| Ejercicios de Programación Funcional - Clase 2
+Este módulo contiene ejercicios para practicar conceptos de programación funcional en Elm.
+Cada función debe implementarse usando principios de programación funcional.
+
+Nota: Las funciones que podrían fallar (como buscar elementos) devuelven valores por defecto (0)
+en lugar de usar Maybe. Trabajamos con List en lugar del List de Scala.
+
+-}
+
+
+-- Concatenar
+-- Dadas 2 listas, devuelve una lista que es la concatenación de ambas.
+-- Usar recursión
+
+concatenar : List Int -> List Int -> List Int
+concatenar lista1 lista2 =
+    case lista1 of
+        [] -> lista2
+        x :: xs -> x :: concatenar xs lista2
+
+
+-- Buscar
+-- Dada una lista y una función de comparación, devuelve el valor que cumple la condición.
+-- Devuelve 0 si la lista está vacía.
+
+buscar : List Int -> (Int -> Int -> Bool) -> Int
+buscar lista com =
+    case lista of
+        [] -> 0
+        x :: xs -> List.foldl (\y acum -> if com y acum then y else acum) x xs
+
+
+-- Busca el Máximo
+-- Encuentra el valor máximo en una lista
+
+max : List Int -> Int
+max lista =
+    buscar lista (\a b -> a > b)
+
+
+-- Busca el Mínimo
+-- Encuentra el valor mínimo en una lista
+
+min : List Int -> Int
+min lista =
+    buscar lista (\a b -> a < b)
+
+
+-- Filtra la lista de valores mayores que el valor e pasado por parámetro
+
+maximos : List Int -> Int -> List Int
+maximos lista e =
+    List.filter (\x -> x > e) lista
+
+
+-- Filtra la lista de valores menores que el valor e pasado por parámetro
+
+minimos : List Int -> Int -> List Int
+minimos lista e =
+    List.filter (\x -> x < e) lista
+
+
+-- Ordena los valores de una lista utilizando quicksort
+
+quickSort : List Int -> List Int
+quickSort xs =
+    case xs of
+        [] ->
+            []
+
+        pivot :: resto ->
+            let
+                menores = List.filter (\x -> x < pivot) resto
+                mayores = List.filter (\x -> x >= pivot) resto
+            in
+            quickSort menores ++ [pivot] ++ quickSort mayores
+            -- TODO: Implementar quicksort recursivamente
+            -- 1. Dividir resto en menores y mayores que pivot
+            -- 2. Ordenar recursivamente ambas particiones
+            -- 3. Concatenar: (menores ordenados) ++ [pivot] ++ (mayores ordenados)
+
+
+-- Obtiene un elemento en la posición n (empezando desde 0)
+-- Devuelve 0 si la posición está fuera de rango
+
+obtenerElemento : List Int -> Int -> Int
+obtenerElemento lista posicion =
+    case (lista, posicion) of
+        ([], _) -> 0
+        (x :: xs, 0) -> x
+        (x :: xs, n) -> obtenerElemento xs (n - 1)
+
+
+-- Busca la mediana
+-- En el ámbito de la estadística, la mediana representa el
+-- valor de la variable de posición central en un conjunto de datos ordenados.
+-- Devuelve 0 si la lista está vacía.
+
+mediana : List Int -> Int
+mediana lista =
+    let
+        ordenada = quickSort lista
+        len = List.length ordenada
+    in
+    if len == 0 then 0
+    else obtenerElemento ordenada (len // 2)
+
+
+-- Cuenta los elementos
+
+contar : List Int -> Int
+contar lista = List.length lista
+
+
+-- Acumula los elementos
+
+acc : List Int -> Int
+acc lista = List.sum lista
+
+
+-- Filtra los elementos de la lista xs según la función p
+
+filtrar : List Int -> (Int -> Bool) -> List Int
+filtrar xs p =
+    List.filter p xs
+
+
+-- Filtra los elementos pares usando la función filtrar
+
+filtrarPares : List Int -> List Int
+filtrarPares xs =
+    -- Pista: Usar modBy 2 para verificar números pares
+    filtrar xs (\x -> modBy 2 x == 0)
+
+
+-- Filtra los elementos múltiplos de 3 usando filtrar
+
+filtrarMultiplosDeTres : List Int -> List Int
+filtrarMultiplosDeTres xs =
+    filtrar xs (\x -> modBy 3 x == 0)
+
+
+-- Acumula los elementos aplicándoles fx
+
+acumular : List Int -> (Int -> Int) -> Int
+acumular lista fx =
+    List.sum (List.map fx lista)
+
+
+-- Acumula todos los elementos de una lista usando acumular (función identidad)
+
+acumularUnidad : List Int -> Int
+acumularUnidad lista =
+    -- Pista: (\x -> x)
+    acumular lista (\x -> x)
+
+
+-- Acumula el doble de los elementos de una lista usando acumular
+
+acumularDoble : List Int -> Int
+acumularDoble lista =
+    -- Pista: (\x -> x * 2)
+    acumular lista (\x -> x * 2)
+
+
+-- Acumula el cuadrado de los elementos de una lista usando acumular
+
+acumularCuadrado : List Int -> Int
+acumularCuadrado lista =
+    -- Pista: (\x -> x * x)
+    acumular lista (\x -> x * x)
+
+
+-- Transforma la lista a una lista de otro tipo
+-- Esto es equivalente a la función map de Scala
+
+transformar : List Int -> (Int -> a) -> List a
+transformar lista fx =
+    List.map fx lista
+
+
+-- Retorna true si un elemento existe en la lista
+
+existe : List Int -> Int -> Bool
+existe lista nro =
+    List.member nro lista
+
+
+-- Une 2 listas pasadas por parámetros pero ignora los repetidos
+
+unirOfSet : List Int -> List Int -> List Int
+unirOfSet lista otraLista =
+    -- Vas a necesitar una función auxiliar para remover duplicados
+    removerDuplicados (lista ++ otraLista)
+
+
+-- Función auxiliar para remover duplicados de una lista
+
+removerDuplicados : List Int -> List Int
+removerDuplicados lista =
+    List.reverse (List.foldl (\x accList -> if List.member x accList then accList else x :: accList) [] lista)
+
+
+-- OPCIONAL: Subconjuntos
+-- Dada una lista de enteros, retorna una lista con todos los posibles subconjuntos
+-- Por ejemplo: [1,2,3] -> [[], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]]
+
+subSets : List Int -> List (List Int)
+subSets lista =
+    case lista of
+        [] ->
+            [ [] ]
+
+        x :: xs ->
+            []
+
+
+-- OPCIONAL: Cortar
+-- Dada una lista de enteros y un número entero n, retorna subconjuntos con n elementos
+-- Ejemplo: [1,2,3,4,5] y 2 -> [[1,2], [3,4], [5]]
+
+cortar : List Int -> Int -> List (List Int)
+cortar lista n =
+    []
+
+
+-- Función auxiliar para tomar los primeros n elementos de una lista
+
+tomar : Int -> List a -> List a
+tomar n lista =
+    []
+
+
+-- Función auxiliar para saltar los primeros n elementos de una lista
+
+saltar : Int -> List a -> List a
+saltar n lista =
+    []
+
+
+-- Ejemplos de uso y funciones de prueba
+-- Podés usar estos para probar tus implementaciones
+
+ejemplos : List String
+ejemplos =
+    [ "max [1,2,3,4,5] debería devolver 5"
+    , "min [1,2,3,4,5] debería devolver 1"
+    , "maximos [1,2,3,4,5] 3 debería devolver [4,5]"
+    , "minimos [1,2,3,4,5] 3 debería devolver [1,2]"
+    , "quickSort [3,1,4,1,5,9,2,6] debería devolver [1,1,2,3,4,5,6,9]"
+    , "contar [1,2,3,4,5] debería devolver 5"
+    , "acc [1,2,3,4,5] debería devolver 15"
+    , "filtrarPares [1,2,3,4,5,6] debería devolver [2,4,6]"
+    , "filtrarMultiplosDeTres [1,2,3,6,9,10] debería devolver [3,6,9]"
+    , "acumularDoble [1,2,3] debería devolver 12 (2+4+6)"
+    , "acumularCuadrado [1,2,3] debería devolver 14 (1+4+9)"
+    , "unir [1,2] [3,4] debería devolver [1,2,3,4]"
+    , "existe [1,2,3] 2 debería devolver True"
+    , "existe [1,2,3] 4 debería devolver False"
+    ]
