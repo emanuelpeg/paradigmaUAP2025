377363432ab813d4efa2777d95bfd7cb594e23f4
felipecorreacamio <felipecorreacamio@gmail.com>
2025-10-18 23:18:34 -0300

    guia 2 terminada

--- funcional/ejercicios/ejercicio2/src/Clase2.elm
+++ funcional/ejercicios/ejercicio2/src/Clase2.elm
@@ -27,68 +27,74 @@ en lugar de usar Maybe. Trabajamos con List en lugar del List de Scala.
 
 
 
--- Concatenar
+-- EJERCICIO 1: Concatenar
 -- Dadas 2 listas, devuelve una lista que es la concatenación de ambas.
 -- Usar recursión
 
-
 concatenar : List Int -> List Int -> List Int
 concatenar lista1 lista2 =
-    []
+    case lista1 of
+        [] ->
+            lista2
+        cabeza :: cola ->
+            cabeza :: concatenar cola lista2
 
 
 
--- Buscar
+-- EJERCICIO 1: Buscar
 -- Dada una lista y una función de comparación, devuelve el valor que cumple la condición.
 -- Devuelve 0 si la lista está vacía.
 
 
 buscar : List Int -> (Int -> Int -> Bool) -> Int
 buscar lista com =
-    0
+    case lista of   
+        [] ->
+            0 
+        [x] ->
+            x
+        x :: xs ->
+            let
+                mejorDelResto = buscar xs com
+            in
+            if com x mejorDelResto then
+                x
+            else
+                mejorDelResto
 
 
 
--- Busca el Máximo
+-- EJERCICIO 2: Busca el Máximo
 -- Encuentra el valor máximo en una lista
-
-
 max : List Int -> Int
 max lista =
-    0
+    buscar lista (\a b -> a > b)
 
-
-
--- Busca el Mínimo
+-- EJERCICIO 2: Busca el Mínimo
 -- Encuentra el valor mínimo en una lista
-
-
 min : List Int -> Int
 min lista =
-    0
+    buscar lista (\a b -> a < b)
 
 
 
--- Filtra la lista de valores mayores que el valor e pasado por parámetro
-
+-- EJERCICIO 3: Filtra la lista de valores mayores que el valor e pasado por parámetro
 
 maximos : List Int -> Int -> List Int
 maximos lista e =
-    []
+    List.filter (\x -> x > e) lista
 
 
 
--- Filtra la lista de valores menores que el valor e pasado por parámetro
-
+-- EJERCICIO 3: Filtra la lista de valores menores que el valor e pasado por parámetro
 
 minimos : List Int -> Int -> List Int
 minimos lista e =
-    []
+    List.filter (\x -> x < e) lista
 
 
 
--- Ordena los valores de una lista utilizando quicksort
-
+-- EJERCICIO 4: Ordena los valores de una lista utilizando quicksort
 
 quickSort : List Int -> List Int
 quickSort xs =
@@ -97,163 +103,181 @@ quickSort xs =
             []
 
         pivot :: resto ->
-            -- TODO: Implementar quicksort recursivamente
-            -- 1. Dividir resto en menores y mayores que pivot
-            -- 2. Ordenar recursivamente ambas particiones
-            -- 3. Concatenar: (menores ordenados) ++ [pivot] ++ (mayores ordenados)
-            []
+            let
+                menores = List.filter (\n -> n < pivot) resto
+                mayores = List.filter (\n -> n >= pivot) resto
+            in
+            quickSort menores ++ [pivot] ++ quickSort mayores
 
 
 
--- Obtiene un elemento en la posición n (empezando desde 0)
+-- EJERCICIO 5: Obtiene un elemento en la posición n (empezando desde 0)
 -- Devuelve 0 si la posición está fuera de rango
-
 
 obtenerElemento : List Int -> Int -> Int
 obtenerElemento lista posicion =
-    0
+    case (lista, posicion) of
+        ([], _) ->
+            0
+        (x :: _, 0) ->
+            x
+        (x :: xs, n) ->
+            if n < 0 then
+                0
+            else
+                obtenerElemento xs (n - 1)
 
 
 
--- Busca la mediana
+-- EJERCICIO 6: Busca la mediana
 -- En el ámbito de la estadística, la mediana representa el
 -- valor de la variable de posición central en un conjunto de datos ordenados.
 -- Devuelve 0 si la lista está vacía.
 
-
 mediana : List Int -> Int
 mediana lista =
-    0
+    case lista of
+        [] ->
+            0
+        _ ->
+            let
+                listaOrdenada = quickSort lista
+                longitud = List.length listaOrdenada
+                indiceMedia = longitud // 2
+            in
+            if modBy 2 longitud == 1 then
+                obtenerElemento listaOrdenada indiceMedia
+            else
+                obtenerElemento listaOrdenada (indiceMedia - 1)
 
 
 
--- Cuenta los elementos
-
+-- EJERCICIO 7: Cuenta los elementos
 
 contar : List Int -> Int
 contar lista =
-    0
+    List.length lista
 
 
 
--- Acumula los elementos
-
+-- EJERCICIO 7: Acumula los elementos
 
 acc : List Int -> Int
 acc lista =
-    0
+    List.sum lista
 
 
 
--- Filtra los elementos de la lista xs según la función p
-
+-- EJERCICIO 8: Filtra los elementos de la lista xs según la función p
 
 filtrar : List Int -> (Int -> Bool) -> List Int
 filtrar xs p =
-    []
+    List.filter p xs
 
 
 
--- Filtra los elementos pares usando la función filtrar
-
+-- EJERCICIO 8: Filtra los elementos pares usando la función filtrar
 
 filtrarPares : List Int -> List Int
 filtrarPares xs =
-    -- Pista: Usar modBy 2 para verificar números pares
-    []
+    filtrar xs (\numero -> modBy 2 numero == 0)
 
 
 
--- Filtra los elementos múltiplos de 3 usando filtrar
-
+-- EJERCICIO 8: Filtra los elementos múltiplos de 3 usando filtrar
 
 filtrarMultiplosDeTres : List Int -> List Int
 filtrarMultiplosDeTres xs =
-    []
+    filtrar xs (\numero -> modBy 3 numero == 0)
 
 
 
--- Acumula los elementos aplicándoles fx
-
+-- EJERCICIO 9: Acumula los elementos aplicándoles fx
 
 acumular : List Int -> (Int -> Int) -> Int
 acumular lista fx =
-    0
+    List.sum (List.map fx lista)
 
 
 
--- Acumula todos los elementos de una lista usando acumular (función identidad)
-
+-- EJERCICIO 9: Acumula todos los elementos de una lista usando acumular (función identidad)
 
 acumularUnidad : List Int -> Int
 acumularUnidad lista =
-    -- Pista: (\x -> x)
-    0
+    acumular lista (\x -> x)
 
 
 
--- Acumula el doble de los elementos de una lista usando acumular
-
+-- EJERCICIO 9: Acumula el doble de los elementos de una lista usando acumular
 
 acumularDoble : List Int -> Int
 acumularDoble lista =
-    -- Pista: (\x -> x * 2)
-    0
+    acumular lista (\x -> x * 2)
 
 
 
 -- Acumula el cuadrado de los elementos de una lista usando acumular
 
+-- EJERCICIO 9: Acumula el cuadrado de los elementos de una lista usando acumular
 
 acumularCuadrado : List Int -> Int
 acumularCuadrado lista =
-    -- Pista: (\x -> x * x)
-    0
+    acumular lista (\x -> x * x)
 
 
 
--- Transforma la lista a una lista de otro tipo
+-- EJERCICIO 10: Transforma la lista a una lista de otro tipo
 -- Esto es equivalente a la función map de Scala
-
 
 transformar : List Int -> (Int -> a) -> List a
 transformar lista fx =
-    []
+    List.map fx lista
 
 
 
--- Retorna true si un elemento existe en la lista
-
+-- EJERCICIO 10: Retorna true si un elemento existe en la lista
 
 existe : List Int -> Int -> Bool
 existe lista nro =
-    False
+    List.member nro lista
 
 
 
--- Une 2 listas pasadas por parámetros pero ignora los repetidos
+-- EJERCICIO 10: Une dos listas
 
+unir : List Int -> List Int -> List Int
+unir lista1 lista2 =
+    lista1 ++ lista2
+
+
+
+-- EJERCICIO 11: Une 2 listas pasadas por parámetros pero ignora los repetidos
 
 unirOfSet : List Int -> List Int -> List Int
 unirOfSet lista otraLista =
-    -- Vas a necesitar una función auxiliar para remover duplicados
-    []
+    removerDuplicados (unir lista otraLista)
 
 
 
--- Función auxiliar para remover duplicados de una lista
-
+-- EJERCICIO 11: Función auxiliar para remover duplicados de una lista
 
 removerDuplicados : List Int -> List Int
 removerDuplicados lista =
-    []
+    List.foldl
+        (\elemento acumulador ->
+            if List.member elemento acumulador then
+                acumulador
+            else
+                acumulador ++ [elemento]
+        )
+        []
+        lista
 
 
 
 -- OPCIONAL: Subconjuntos
 -- Dada una lista de enteros, retorna una lista con todos los posibles subconjuntos
 -- Por ejemplo: [1,2,3] -> [[], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]]
-
 
 subSets : List Int -> List (List Int)
 subSets lista =
@@ -262,7 +286,10 @@ subSets lista =
             [ [] ]
 
         x :: xs ->
-            []
+            let
+                subconjuntosSinX = subSets xs
+            in
+            subconjuntosSinX ++ List.map (\subset -> x :: subset) subconjuntosSinX
 
 
 
@@ -270,28 +297,35 @@ subSets lista =
 -- Dada una lista de enteros y un número entero n, retorna subconjuntos con n elementos
 -- Ejemplo: [1,2,3,4,5] y 2 -> [[1,2], [3,4], [5]]
 
-
 cortar : List Int -> Int -> List (List Int)
 cortar lista n =
-    []
+    if n <= 0 || List.isEmpty lista then
+        []
+    else
+        let
+            pedazo = tomar n lista
+            resto = saltar n lista
+        in
+        if List.isEmpty pedazo then
+            []
+        else
+            pedazo :: cortar resto n
 
 
 
 -- Función auxiliar para tomar los primeros n elementos de una lista
 
-
 tomar : Int -> List a -> List a
 tomar n lista =
-    []
+    List.take n lista
 
 
 
 -- Función auxiliar para saltar los primeros n elementos de una lista
 
-
 saltar : Int -> List a -> List a
 saltar n lista =
-    []
+    List.drop n lista
 
 
 
